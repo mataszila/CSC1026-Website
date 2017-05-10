b4b3f13d4ca2a458b9d1571db7bd7ae5f0767966
mataszilaitis <matas.zilaitis@gmail.com>
2017-05-10 14:35:23 +0100

    Website

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML = 
+typeof [1,2,3,4] + "<br>" +
+typeof {name:'john', age:34} + "<br>" +
+typeof function myFunc(){};
+
+}
